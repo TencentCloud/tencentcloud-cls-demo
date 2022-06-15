b0b787e96c439f9136c3c84e790cddabb2eac5c8
herrylv <herrylv@tencent.com>
2022-06-15 14:13:23 +0800

    commit message

---
+++ TencentCloudLogProducer/TencentCloudLogProducer/Log.m
@@ -0,0 +1,47 @@
+
+
+#import <Foundation/Foundation.h>
+#import "Log.h"
+#import "TimeUtils.h"
+
+@interface Log ()
+
+@end
+
+@implementation Log
+
+- (id) init
+{
+    if (self = [super init])
+    {
+        self->logTime = [TimeUtils getTimeInMilliis];
+        self->content = [NSMutableDictionary dictionary];
+
+    }
+
+    return self;
+}
+
+- (void)PutContent:(NSString *) key value:(NSString *)value
+{
+    if (key && value) {
+        [self->content setObject:value forKey:key];
+    }
+}
+
+- (NSMutableDictionary *)getContent
+{
+    return self->content;
+}
+
+- (void)SetTime:(unsigned int) logTime
+{
+    self->logTime = logTime;
+}
+
+- (unsigned int)getTime
+{
+    return self->logTime;
+}
+
+@end
